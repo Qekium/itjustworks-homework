fd2590da702e5e031721e055d957c6ef7e8e5ad8
Qekium <pilotearesolutioner@gmail.com>
2022-04-20 22:09:07 +0400

    here it goes ig

---
+++ src/com/company/celheit.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class celheit {
+
+    public static void main(String[] args) {
+        byte cinput = 22; // provide the desirable number you'd like to convert;
+        double cons1 = 1.8; // constant #1
+        byte cons2 = 32; // constant #2
+        double foutput = cinput * cons1 + cons2; //tl;dr
+        String degc = "°C";
+        String eqls = "equals";
+        String degf = "°F";
+        System.out.println(cinput + degc);
+        System.out.println(eqls);
+        System.out.println(foutput + degf);
+        // i couldn't make it any simpler nor more sophististicated, i hope it gets by
+        // i will fix it eventually and make a commit on my github rep, just you wait
+        // (not that anyone will ever read it ever anyway ig)
+    }
+}
+
